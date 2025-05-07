3db120be8bfc06a29676c44bf790708f742c8e4b
christian <christian@backupfailed.com>
2025-05-06 22:46:16 -0700

    lots of changes-pushing to main anyways

---
+++ src/content.config.ts
@@ -0,0 +1,17 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from 'astro:content';
+import { glob, file } from 'astro/loaders';
+
+
+// 3. Define your collection(s)
+const posts = defineCollection({
+    loader: glob({ pattern: "**/*.md", base: "./content/posts" }),
+    schema: z.object({
+        title: z.string(),
+        date: z.string(),
+        tags: z.array(z.string()),
+    }),
+});
+
+// 4. Export a single `collections` object to register your collection(s)
+export const collections = { posts };
