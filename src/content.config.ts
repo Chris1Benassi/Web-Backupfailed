cdc0f37f5c78794a7f8589a681465afefafd7dd9
christian <christian@backupfailed.com>
2025-05-07 20:21:24 -0700

    wipe it all

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,16 +1,14 @@
 import { defineCollection, z } from 'astro:content';
-import { glob, file } from 'astro/loaders';
 
-
-// 3. Define your collection(s)
 const posts = defineCollection({
-    loader: glob({ pattern: "**/*.md", base: "./content/posts" }),
     schema: z.object({
         title: z.string(),
         date: z.string(),
         tags: z.array(z.string()),
+        image: z.string().optional(),
+        excerpt: z.string().optional(),
+        slug: z.string().optional(),
     }),
 });
 
-// 4. Export a single `collections` object to register your collection(s)
 export const collections = { posts };
