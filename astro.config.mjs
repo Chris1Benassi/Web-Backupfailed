f5666d190732daf8378a242de274f4a50acac95b
Chris1Benassi <christian@backupfailed.com>
2025-07-24 15:30:42 -0700

    push changes to address URL linking

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import typography from '@tailwindcss/typography';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://backupfailed.com',
     vite: {
         plugins: [tailwindcss()]
     },
